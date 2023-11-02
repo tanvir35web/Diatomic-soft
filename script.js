e21898405961fc87aef5272319eb8ec0cd8ad070
tanvir35web <islammdtanvirul@gmail.com>
2023-11-02 21:33:05 +0600

    nav bug fixed

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const menuBtn = document.querySelector(".menu-btn");
 const items = document.querySelector(".items");
+const navList = document.querySelector(".nav-list");
 
 menuBtn.addEventListener("click", () => {
     items.classList.toggle("active");
-
 });
+
+// function linkAccount(){
+//     items.classList.remove("active")
+// }
+// navList.forEach(n => n.addEventListener("click", linkAccount))
+
