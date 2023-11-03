ce21f136126f0071d6bd5d229f217181ce9e4e75
tanvir35web <islammdtanvirul@gmail.com>
2023-11-03 23:13:00 +0600

    project section add for web and mobile view both

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 
 // ............scroll reveal animation ............
 
-
 ScrollReveal({
     reset: true,
-    distance: '160px',
-    duration: 1500,
-    delay: 200
+    distance: '70px',
+    duration: 800,
+    delay: 100
 });
 
 ScrollReveal().reveal(".main-heading", {delay: 100, origin: "left"});
 ScrollReveal().reveal(".auto-heading", {delay: 100, origin: "top", interval: 200});
-ScrollReveal().reveal("img, p", {delay: 100, origin: "bottom"});
+ScrollReveal().reveal("img", {delay: 100, origin: "right"});
+ScrollReveal().reveal("p", {delay: 100, origin: "left"});
+ScrollReveal().reveal("h1", {delay: 100, origin: "top"});
+ScrollReveal().reveal(".project", {delay: 100, origin: "bottom"});
 ScrollReveal().reveal(".right-service-odd, .right-service", {delay: 100, origin: "left"});
-ScrollReveal().reveal("img, p", {delay: 100, origin: "bottom"});
 ScrollReveal().reveal(".navbar .left", {delay: 100, origin: "top", interval: 200});
 
 
